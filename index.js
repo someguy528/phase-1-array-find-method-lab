f453d583d5675ff2c15b24aea42df27f491503f5
Brandon Castro <castroyou999@gmail.com>
2022-09-13 15:57:11 -0400

    commit tree 6db4941f691e70b1259b57bbec44f4f835742770

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+//function superbowlWin(array){
+//     for (let obj of array){
+// if(obj.result === "W"){
+//     return obj.year
+// }
+//     }
+// }
+
+function superbowlWin(array){
+    const found = array.find(obj => obj.result ==="W");
+    return !!found ? found.year:undefined;
+}
